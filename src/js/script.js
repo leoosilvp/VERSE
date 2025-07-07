1db4245d09cfa1f647f5e5066fef319692be41f4
leoosilvp <leo965679@gmail.com>
2025-07-06 23:31:00 -0300

    8 a- criando menu sanduiche

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+// menu sanduiche
+    const mobileMenu = document.getElementById('mobileMenu');
+    const navLinks = document.getElementById('navLinks');
+
+    mobileMenu.addEventListener('click', function () {
+        this.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+
+// Close mobile menu when clicking on a link
+    document.querySelectorAll('.nav-links a').forEach(link => {
+        link.addEventListener('click', function () {
+            mobileMenu.classList.remove('active');
+            navLinks.classList.remove('active');
+        });
+    })
+
+
 // carroussel fots
     const carroussel = document.querySelector('.carroussel-container');
     const slides = document.querySelectorAll('.carroussel-slide');
